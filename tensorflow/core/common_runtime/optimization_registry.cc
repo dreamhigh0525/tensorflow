dreamhigh0525/tensorflow

commit 2d053fc2d9d8f524dcda0ec96c237f0f6aa21697

Log num nodes and edges before each pass.
This is log level 1 because log level 2 dumps graphs.

PiperOrigin-RevId: 493962827

tensorflow/core/common_runtime/optimization_registry.cc
@@ -67,6 +67,8 @@ Status OptimizationPassRegistry::RunGrouping(
       VLOG(1) << "Running optimization phase " << phase.first;
       for (auto& pass : phase.second) {
         VLOG(1) << "Running optimization pass: " << pass->name();
+        VLOG(1) << "Graph #nodes " << (*options.graph)->num_nodes()
+                << " #edges " << (*options.graph)->num_edges();
 
         tensorflow::metrics::ScopedCounter<2> pass_timings(
             tensorflow::metrics::GetGraphOptimizationCounter(),
@@ -86,6 +88,10 @@ Status OptimizationPassRegistry::RunGrouping(
     group_timings.ReportAndStop();
   }
   VLOG(1) << "Finished optimization of a group " << grouping;
+  if (group != groups_.end()) {
+    VLOG(1) << "Graph #nodes " << (*options.graph)->num_nodes() << " #edges "
+            << (*options.graph)->num_edges();
+  }
   if (VLOG_IS_ON(3) ||
       (VLOG_IS_ON(2) && grouping == Grouping::POST_REWRITE_FOR_EXEC)) {
     std::string prefix = strings::StrCat(options.debug_filename_prefix,

commit ad6ab4e6bb762b4cb4fb65749b3b37466434b68d

Fix segmentation fault when running with VLOG level >= 1. Ensure `options.graph` isn't null before dereferencing it.
The error was introduced in
https://github.com/tensorflow/tensorflow/commit/2d053fc2d9d8f524dcda0ec96c237f0f6aa21697

PiperOrigin-RevId: 508490664

tensorflow/core/common_runtime/optimization_registry.cc
@@ -67,9 +67,10 @@ Status OptimizationPassRegistry::RunGrouping(
       VLOG(1) << "Running optimization phase " << phase.first;
       for (auto& pass : phase.second) {
         VLOG(1) << "Running optimization pass: " << pass->name();
-        VLOG(1) << "Graph #nodes " << (*options.graph)->num_nodes()
-                << " #edges " << (*options.graph)->num_edges();
-
+        if (options.graph) {
+          VLOG(1) << "Graph #nodes " << (*options.graph)->num_nodes()
+                  << " #edges " << (*options.graph)->num_edges();
+        }
         tensorflow::metrics::ScopedCounter<2> pass_timings(
             tensorflow::metrics::GetGraphOptimizationCounter(),
             {kGraphOptimizationCategory, pass->name()});
@@ -88,7 +89,7 @@ Status OptimizationPassRegistry::RunGrouping(
     group_timings.ReportAndStop();
   }
   VLOG(1) << "Finished optimization of a group " << grouping;
-  if (group != groups_.end()) {
+  if (options.graph && group != groups_.end()) {
     VLOG(1) << "Graph #nodes " << (*options.graph)->num_nodes() << " #edges "
             << (*options.graph)->num_edges();
   }